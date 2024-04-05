04d922f59d46ae450e7860f77fcbb26e3a70602f
BitBurstAlpha <dilshansamarakoon2001@gmail.com>
2024-04-05 10:23:15 +0530

    feat(backend): add current user route

--- apps/server/src/routes/user.routes.ts
+++ apps/server/src/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import validator from '../middleware/validator';
 import { RegisterUserSchema } from '../schemas/user.schema';
-import { UserRegisterHandler } from '../controllers/user.controllers';
+import {
+    GetCurrentUserHandler,
+    UserRegisterHandler,
+} from '../controllers/user.controllers';
+import { verifyJwt } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
 router.post('/register', validator(RegisterUserSchema), UserRegisterHandler);
+router.get('/current-user', verifyJwt, GetCurrentUserHandler);
 
 export default router;
