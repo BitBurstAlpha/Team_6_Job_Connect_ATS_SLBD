7dd26d62becaba144c26963a112a0dfb8ed88660
BDTech1 <abusufians808@gmail.com>
2024-05-01 22:18:15 +0530

    feat: reupdate server domain for client and frontend

--- apps/server/src/controllers/auth.controllers.ts
+++ apps/server/src/controllers/auth.controllers.ts
@@ -54,18 +54,13 @@ export const userLoginHandler = async (
         },
     );
 
-    const domainBase = type === 'client' ? 'client' : 'www';
-
     res.cookie('accessToken', accessToken, {
         maxAge: 604800000,
         path: '/',
         httpOnly: true,
         sameSite: config.NODE_ENV === 'production' ? 'none' : 'strict',
         secure: config.NODE_ENV === 'production',
-        domain:
-            config.NODE_ENV === 'production'
-                ? `${domainBase}.slbd.uk`
-                : 'localhost',
+        domain: config.NODE_ENV === 'production' ? '.slbd.uk' : 'localhost',
     });
 
     return res.status(StatusCodes.OK).json({
